8b8258b4ca2fd31edf459c714b539a3bdcf946c6
Angel Morales Cruz <83528788+AngelMorales88@users.noreply.github.com>
2021-05-13 02:06:17 -0500

    correcion =), homework.js

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -77,7 +77,7 @@ Persona.prototype.detalle = function () {
 // Crea el constructor:
 
 function crearInstanciaPersona(nombre, apellido, edad, dir) {
-  var nuevaPersona = new Persona("Juan", "Perez", 22, "Saavedra 123");
+  var nuevaPersona = new Persona(nombre, apellido, edad, dir);
   return nuevaPersona;
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
